70e28a247f16af65bfc25362b6398b5c57c439f1
김상민 <wrt123123123@gmail.com>
2021-05-04 22:16:48 +0900

    5.4

---
+++ ActionVarMenu/app/src/main/java/com/example/actionvarmenu/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.actionvarmenu;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setIcon(R.drawable.icon);
+    }
+
+    @Override
+    public boolean onOptionItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Toast t = Toast.makeText(this, "HOME AS UP CLICK", Toast.LENGTH_SHORT);
+            t.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
